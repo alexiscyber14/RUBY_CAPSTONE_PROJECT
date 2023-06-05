dc8adabb45e36ffb09a5229cd32a5de861b435b7
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-05 09:22:26 +0100

    fix ruby errors

--- item.rb
+++ item.rb
@@ -6,7 +6,7 @@ class Item
 
   def initialize(publish_date)
     @id = rand(1..1000)
-    @publish_date = Date.parse(publish_date) 
+    @publish_date = Date.parse(publish_date)
     @archived = false
   end
 
