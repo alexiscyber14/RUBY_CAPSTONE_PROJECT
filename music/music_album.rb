9e2080082e95a4ce27324e72168cedb1d8e48747
fuadmoin <fuadamoin@gmail.com>
2023-06-06 14:15:46 +0300

    Create genre class.

--- music/music_album.rb
+++ music/music_album.rb
@@ -1,11 +1,14 @@
-class MusicAlbum
+require_relative '../item'
+
+class MusicAlbum < Item
   attr_accessor :on_spotify
 
-  def initialize(on_spotify: false)
+  def initialize(publish_date, on_spotify: false)
     @on_spotify = on_spotify
+    super(publish_date)
   end
 
   def can_be_archived?
-    @on_spotify == true
+    @on_spotify == true && super
   end
 end
