f75b8af445a64899d256f4879eecebee449925d8
Gracerue <munyaradzi045@gmail.com>
2023-06-06 16:06:13 +0200

    Add test and SQL schema data

--- game/author.rb
+++ game/author.rb
@@ -5,7 +5,6 @@ class Author < Item
   attr_accessor :first_name, :last_name, :items
 
   def initialize(first_name, last_name)
-    super()
     @id = rand(1..1000)
     @first_name = first_name
     @last_name = last_name
