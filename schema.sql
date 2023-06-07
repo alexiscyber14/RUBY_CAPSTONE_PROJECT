a0eb8fe737fa7ea0e19e12f09dadb9761d8ff484
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-07 19:35:41 +0100

    add clean sql

--- schema.sql
+++ schema.sql
@@ -1,13 +1,26 @@
 CREATE TABLE items (
   id SERIAL PRIMARY KEY,
   published_date DATE,
-  archived BOOLEAN
+  archived BOOLEAN,
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id),
+  CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES authors(id),
+  CONSTRAINT fk_label_id FOREIGN KEY (label_id) REFERENCES label(id)
 );
 
+CREATE INDEX idx_items_published_date ON items (published_date);
+CREATE INDEX idx_items_genre_id ON items (genre_id);
+CREATE INDEX idx_items_author_id ON items (author_id);
+CREATE INDEX idx_items_label_id ON items (label_id);
+
 CREATE TABLE label (
-  id INT PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(150),
-  color VARCHAR(50)
+  color VARCHAR(50),
+  item_id INT,
+  CONSTRAINT fk_label_item_id FOREIGN KEY (item_id) REFERENCES items(id)
 );
 
 CREATE TABLE book (
@@ -17,7 +30,7 @@ CREATE TABLE book (
   cover_state VARCHAR(50),
   archived BOOLEAN,
   label_id INT,
-  CONSTRAINT fk_label_id FOREIGN KEY (label_id) REFERENCES label(id)
+  CONSTRAINT fk_book_label_id FOREIGN KEY (label_id) REFERENCES label(id)
 );
 
 CREATE INDEX idx_book_label_id ON book (label_id);
@@ -34,7 +47,7 @@ CREATE TABLE games (
   multiplayer BOOLEAN,
   last_played_at DATE,
   author_id INT,
-  CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES authors(id)
+  CONSTRAINT fk_game_author_id FOREIGN KEY (author_id) REFERENCES authors(id)
 );
 
 CREATE INDEX idx_games_author_id ON games (author_id);
@@ -50,7 +63,7 @@ CREATE TABLE music_album (
   on_spotify BOOLEAN NOT NULL,
   publish_date DATE NOT NULL,
   genre_id INT,
-  CONSTRAINT fk_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id)
+  CONSTRAINT fk_music_album_genre_id FOREIGN KEY (genre_id) REFERENCES genre(id)
 );
 
 CREATE INDEX idx_music_album_genre_id ON music_album (genre_id);
@@ -59,8 +72,8 @@ CREATE TABLE associations (
   id SERIAL PRIMARY KEY,
   item_id INT,
   associated_item_id INT,
-  CONSTRAINT fk_item_id FOREIGN KEY (item_id) REFERENCES items(id),
-  CONSTRAINT fk_associated_item_id FOREIGN KEY (associated_item_id) REFERENCES items(id)
+  CONSTRAINT fk_associations_item_id FOREIGN KEY (item_id) REFERENCES items(id),
+  CONSTRAINT fk_associations_associated_item_id FOREIGN KEY (associated_item_id) REFERENCES items(id)
 );
 
 CREATE INDEX idx_associations_item_id ON associations (item_id);
